dfa9b0896c9772d811080ad316fe49b2f04bb018
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-03-26 13:31:19 +0200

    Initial commit

---
+++ server/src/main/java/blog/food/story/BlogFoodStoryApplication.java
@@ -0,0 +1,11 @@
+package blog.food.story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFoodStoryApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BlogFoodStoryApplication.class, args);
+    }
+}
