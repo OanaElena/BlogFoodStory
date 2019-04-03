a313048431c19f2fecbb1203628ee944136cb11d
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-04-03 13:39:30 +0300

    add recipe -- Steps List

--- ui/src/blogfoodstory/add-recipe/add-recipe.component.ts
+++ ui/src/blogfoodstory/add-recipe/add-recipe.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Ingredient} from "../model/ingredient";
 import {FoodService} from "../services/food.service";
 import {MatSelectChange} from "@angular/material";
 import {debounceTime} from "rxjs/operators";
+import {StepRecipes} from "../model/step-recipes";
 
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.css']
+  styleUrls: ['./add-recipe.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddRecipeComponent implements OnInit {
 
   recipeForm: FormGroup;
   ingredientsList: Ingredient[] = [];
   alreadyExistingIngredients: Ingredient[];
+  stepRecipeList: StepRecipes[] = [];
+  stepOrderId = 0;
+  items: FormArray;
+
   constructor(private fb: FormBuilder,
               private foodService: FoodService) { }
+
+  get formRecipe() { return <FormArray>this.recipeForm.get('stepsList'); }
 
   ngOnInit() {
 
@@ -28,7 +36,8 @@ export class AddRecipeComponent implements OnInit {
         ingredientName: new FormControl(),
         ingredientQuantity: new FormControl(),
         ingredientUnitName: new FormControl()
-      })
+      }),
+      stepsList : this.fb.array([])
     });
 
     this.recipeForm.get('ingredientList').valueChanges
@@ -62,5 +71,32 @@ export class AddRecipeComponent implements OnInit {
 
   deleteIngredient(id: number) {
     this.ingredientsList = this.ingredientsList.filter(ingred => ingred.id != id);
+  }
+
+  addNewStep() {
+    let recipeFormLength = this.formRecipe.controls.length;
+    if(recipeFormLength == 0){
+      this.addStep();
+      this.formRecipe.controls[recipeFormLength].get('stepOrderId').setValue(recipeFormLength+1);
+    }else{
+      if(this.formRecipe.controls[recipeFormLength-1].get('stepDescription').value != null || this.formRecipe.controls[recipeFormLength-1].get('coverPhoto').value != null){
+        this.addStep();
+        this.formRecipe.controls[recipeFormLength].get('stepOrderId').setValue(recipeFormLength+1);
+      }
+    }
+  }
+
+  addStep(){
+    this.items = this.recipeForm.get('stepsList') as FormArray;
+    this.items.push(this.createItem());
+  }
+
+  private createItem(): FormGroup {
+    return this.fb.group({
+      stepOrderId: new FormControl(),
+      stepDescription: new FormControl(),
+      coverPhoto: new FormControl(),
+      photoStepName: new FormControl()
+    })
   }
 }
