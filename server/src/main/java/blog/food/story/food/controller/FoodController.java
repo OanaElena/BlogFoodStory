6492c12ea8a2045571b58e3322dd9f8e4c596638
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-06-05 11:52:17 +0300

    tags and test for rebase branch into master

--- server/src/main/java/blog/food/story/food/controller/FoodController.java
+++ server/src/main/java/blog/food/story/food/controller/FoodController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * I have added a comment for git rebase master into branch
+ * I have added a comment for git rebase branch into master
  */
 @RestController
 @RequestMapping("/food")
