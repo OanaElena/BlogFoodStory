b9f13557773ef726c726c7e70541384cf77f4069
Leanca Oana <oana.leanca@kepler-rominfo.com>
2019-06-10 16:26:06 +0300

    merge branch into master

--- server/src/main/java/blog/food/story/food/controller/FoodController.java
+++ server/src/main/java/blog/food/story/food/controller/FoodController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- * I have added a comment for git rebase master into branch -check for conflicts
+ * Merge branch into master commit and see what happens
  */
 @RestController
 @RequestMapping("/food")
